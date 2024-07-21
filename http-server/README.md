1dd093f32bb13dd4ace71a89a3e09fc25b95ffdd
Vadim Shender <vadim@shender.org>
2024-07-21 22:37:42 +0100

    Respond with 200

--- http-server/README.md
+++ http-server/README.md
@@ -23,3 +23,56 @@ $ dune exec ./http_server.exe
 ```
 
 Then, the tester will try to connect to your server on port 4221.  The connection must succeed for you to pass this stage.
+
+
+## Respond with 200
+
+In this stage, your server will respond to an HTTP request with a `200` response.
+
+### HTTP response
+
+An HTTP response is made up of three parts, each separated by a [CRLF](https://developer.mozilla.org/en-US/docs/Glossary/CRLF) (`\r\n`):
+
+1. Status line.
+1. Zero or more headers, each ending with a CRLF.
+1. Optional response body.
+
+In this stage, your server's response will only contain a status line.  Here's the response your server must send:
+```
+HTTP/1.1 200 OK\r\n\r\n
+```
+
+Here's a breakdown of the response:
+```
+// Status line
+HTTP/1.1  // HTTP version
+200       // Status code
+OK        // Optional reason phrase
+\r\n      // CRLF that marks the end of the status line
+// Headers (empty)
+\r\n      // CRLF that marks the end of the headers
+// Response body (empty)
+```
+
+### Tests
+
+The tester will execute your program like this:
+```
+$ dune exec ./http_server.exe
+```
+
+The tester will then send an HTTP `GET` request to your server:
+```
+$ curl -v http://localhost:4221
+```
+
+Your server must respond to the request with the following response:
+```
+HTTP/1.1 200 OK\r\n\r\n
+```
+
+### Notes
+
+- You can ignore the contents of the request.  We'll cover parsing requests in later stages.
+- For more information about HTTP responses, see the [MDN Web Docs on HTTP responses](https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages#http_responses) or the [HTTP/1.1 specification](https://datatracker.ietf.org/doc/html/rfc9112#name-message).
+- This challenge uses HTTP/1.1.
--- http-server/http_server.ml
+++ http-server/http_server.ml
@@ -1,5 +1,67 @@
 open Unix
 
+module HTTPResponse = struct
+  open Faraday
+
+  (** Type of HTTP responses. *)
+  type t = {
+    version : int * int;
+    status  : int;
+    body    : string;
+  }
+
+  let status_codes = [
+    (200, "200 OK");
+  ]
+
+  (** [make ?version ?status ?body ()] creates an HTTP response. *)
+  let make ?(version=(1, 1)) ?(status=200) ?(body="") () =
+    { version; status; body }
+
+  (** [serialize sr r] serializes the given HTTP response [r]. *)
+  let serialize sr r =
+    let major, minor = r.version
+    and status_code =
+      try
+        List.assoc r.status status_codes
+      with
+      | Not_found ->
+        List.assoc 500 status_codes
+    in
+    write_string sr (Printf.sprintf "HTTP/%d.%d " major minor);
+    write_string sr status_code;
+    write_string sr "\r\n";
+    write_string sr "\r\n";
+    write_string sr r.body
+end
+
+(** [send_response sock sr] sends the serialized HTTP response [sr] to
+    [sock]. *)
+let send_response sock t =
+  let open Faraday in
+  let shutdown () =
+    close t;
+    ignore (Faraday.drain t)
+  in
+  let rec loop () =
+    match Faraday.operation t with
+    | `Writev iovecs ->
+      let { buffer; off; len } = List.hd iovecs in
+      let n = write_bigarray sock buffer off len in
+      if n >= 0 then shift t n else shutdown ();
+      loop ()
+    | `Close -> ()
+    | `Yield -> assert false
+  in loop ()
+
+(** [handle_request sock] handles an incoming HTTP request. *)
+let handle_request sock =
+  let resp = HTTPResponse.make () in
+  let sr = Faraday.create 0x1000 in
+  HTTPResponse.serialize sr resp;
+  Faraday.close sr;
+  send_response sock sr
+
 (** [server ()] runs an HTTP server. *)
 let server () =
   (* Create a TCP socket bound to the given address. *)
@@ -11,8 +73,20 @@ let server () =
 
   while true do
     (* Wait for a connection *)
-    let _, _ = accept ~cloexec:true ssock in
-    ()
+    let csock, caddr = accept ~cloexec:true ssock in
+    let cni = getnameinfo caddr [] in
+    Printf.printf "Connection from %s:%s\n%!" cni.ni_hostname cni.ni_service;
+
+    begin
+      try
+        (* Handle a request. *)
+        handle_request csock;
+        (* Close the connection to the client. *)
+        shutdown csock SHUTDOWN_ALL
+      with
+        e -> Printf.printf "Error: %s\n%!" @@ Printexc.to_string e
+    end;
+    close csock
   done
 
 let () = server ()
